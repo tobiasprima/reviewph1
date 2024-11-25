70d4e1fa5c294d68711b7ccbe6194b4bfab451a1
Tobias Prima <primatjahja@gmail.com>
2024-11-25 20:40:23 +0800

    feat: first

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
+// 1. Functions to print numbers and letters concurrently
+func printNumbers() {
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
+}
+
+func printLetters() {
+	for ch := 'a'; ch <= 'j'; ch++ {
+		fmt.Println(string(ch))
+	}
+}
+
 func main(){
-	
+	// 1
+	printNumbers()
+	printLetters()
 }
